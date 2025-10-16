3582bbd9e298dd3ac490653247abbb9a89fb6f38
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-10-16 15:42:51 +0200

    Add a class to send emails

---
+++ App/Mail.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Mailgun\Mailgun;
+use App\Config;
+
+/**
+ * Mail class for sending emails via Mailgun
+ */
+class Mail
+{
+    /**
+     * Send an email via Mailgun
+     *
+     * @param string $to Recipient email
+     * @param string $subject Email subject
+     * @param string $text Plain text body
+     * @param string|null $html HTML body (optional)
+     *
+     * @return bool True if sent successfully, false otherwise
+     */
+    public static function send(string $to, string $subject, string $text, ?string $html = null): bool
+    {
+        try {
+            // Create Mailgun client
+            $mg = Mailgun::create(
+                getenv('MAILGUN_API_KEY') ?: Config::MAILGUN_API_KEY
+            );
+
+            // Message data
+            $data = [
+                'from'    => 'Your App <postmaster@' . Config::MAILGUN_DOMAIN . '>',
+                'to'      => $to,
+                'subject' => $subject,
+                'text'    => $text,
+            ];
+
+            // Add HTML content if provided
+            if ($html !== null) {
+                $data['html'] = $html;
+            }
+
+            // Send the message
+            $mg->messages()->send(Config::MAILGUN_DOMAIN, $data);
+
+            return true;
+        } catch (\Exception $e) {
+            error_log('Mailgun send failed: ' . $e->getMessage());
+            return false;
+        }
+    }
+}
