06af728ebfc754cb510f055bb6bfc8c7752d0652
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-10-17 11:31:27 +0200

    Create and display the password reset form

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -41,6 +41,11 @@ class Password extends \Core\Controller
 
         $user = User::findByPasswordReset($token);
 
-        var_dump($user);
+        if ($user) {
+            View::renderTemplate('Password/reset.html');
+        } else {
+            echo 'password reset token invalid';
+        }
+
     }
 }
