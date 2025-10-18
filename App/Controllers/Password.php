7e965209c82c31847d51077a41d1368fa15e32c5
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-10-18 06:47:45 +0200

    Remove duplicated code and add an expired token view

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -39,15 +39,13 @@ class Password extends \Core\Controller
     {
         $token = $this->route_params['token'];
 
+        $user = $this->getUserOrExit($token);
+
         $user = User::findByPasswordReset($token);
 
-        if ($user) {
-            View::renderTemplate('Password/reset.html', [
-                'token' => $token
-            ]);
-        } else {
-            echo 'password reset token invalid';
-        }
+        View::renderTemplate('Password/reset.html', [
+             'token' => $token
+         ]);
     }
 
     /**
@@ -59,13 +57,28 @@ class Password extends \Core\Controller
     {
         $token = $_POST['token'];
 
+        $user = $this->getUserOrExit($token);
+
+        echo "reset user's password here";
+    }
+
+    /**
+     * Find the user model associated with the password reset token, or end the request with a message
+     *
+     * @param string $token Password reset token sent to user
+     *
+     * @return mixed User object if found and the token hasn't expired, null otherwise
+     *
+     */
+    protected function getUserOrExit($token)
+    {
         $user = User::findByPasswordReset($token);
 
         if ($user) {
-
-            echo "reset user's password here";
+            return $user;
         } else {
-            echo 'password reset token invalid';
+            View::renderTemplate('Password/token_expired.html');
+            exit;
         }
     }
 }
