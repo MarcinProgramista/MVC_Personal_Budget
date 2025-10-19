9308b3973c7055aa7fd767e5d9a684646535bc6e
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-10-19 06:39:41 +0200

    User profile: let the user update their own details

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\View;
+
+/**
+ * Profile controller
+ */
+class Profile extends Authenticated
+{
+    /**
+     * Show the profile
+     *
+     * @return void
+     */
+    public function showAction()
+    {
+        View::renderTemplate('Profile/show.html');
+    }
+}
