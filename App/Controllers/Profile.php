542496bab46eafedf7e95f52f8a52dcdd4313b7a
Marcin Czapla <marcin.czapla.programista@gmail.com>
2025-10-20 13:58:41 +0200

    Create and display the edite profile form

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -3,22 +3,46 @@
 namespace App\Controllers;
 
 use App\Auth;
+use App\Flash;
 use Core\View;
+use App\Models\User;
 
-/**
- * Profile controller
- */
 class Profile extends Authenticated
 {
+    public function showAction()
+    {
+        $user = Auth::getUser();
+        // Pobranie pełnego obiektu użytkownika z bazy, żeby mieć ID i wszystkie pola
+        $user = User::findByID($user->id);
+
+        View::renderTemplate('Profile/show.html', [
+            'user' => $user
+        ]);
+    }
+
     /**
-     * Show the profile
+     * Show the form for editing the profile
      *
      * @return void
      */
-    public function showAction()
+    public function editAction()
     {
-        View::renderTemplate('Profile/show.html', [
+        View::renderTemplate('Profile/edit.html', [
             'user' => Auth::getUser()
         ]);
     }
+
+    public function updateAction()
+    {
+        $user = Auth::getUser();
+
+        if ($user->updateProfile($_POST)) {
+            Flash::addMessage('Changes saved');
+            $this->redirect('profile/show');
+        }
+
+    }
+
+
+
 }
